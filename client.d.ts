4ecee8db96424aa16e835e11cf118993b9afd41e
CrazyZhang3 <zh6317@foxmal.com>
2025-03-01 20:56:04 +0800

    refactor(types): 重构类型定义并添加虚拟模块声明

--- client.d.ts
+++ client.d.ts
@@ -1,9 +1,4 @@
-declare module 'virtual:uni-pages' {
-  import { PagesConfig as config } from '@team-nw/unplugin-uni-pages/types'
-
-  export default config
-}
-
 declare global {
   const definePage: (typeof import('@team-nw/unplugin-uni-pages/runtime'))['definePage']
 }
+export {}
---
+++ virtual.d.ts
@@ -0,0 +1,6 @@
+declare module 'virtual:uni-pages' {
+  import type { PagesConfig } from '@team-nw/unplugin-uni-pages/types'
+
+  const pages: PagesConfig
+  export default pages
+}
