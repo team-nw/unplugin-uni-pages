2c061ee31845f26f61c33b4a66098d018b017569
CrazyZhang3 <CrazyZhang3@163.com>
2025-07-30 11:23:15 +0800

    v0.2.3

--- client.d.ts
+++ client.d.ts
@@ -1,10 +1,10 @@
-declare global {
-  const definePage: (typeof import('@team-nw/unplugin-uni-pages/runtime'))['definePage']
-}
-
 declare module 'virtual:uni-pages' {
   import type { PagesConfig } from '@team-nw/unplugin-uni-pages/types'
 
   const pages: PagesConfig
   export default pages
+}
+export {}
+declare global {
+  const definePage: (typeof import('@team-nw/unplugin-uni-pages/runtime'))['definePage']
 }
